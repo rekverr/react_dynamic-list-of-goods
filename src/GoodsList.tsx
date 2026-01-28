c004e9cee871515e373526c3cd50371d781921d7
Viktor Shchehelskyi <victorshegelsky@gmail.com>
2026-01-28 22:46:21 +0200

    finished fixed

--- src/GoodsList.tsx
+++ src/GoodsList.tsx
@@ -5,12 +5,14 @@ type Props = {
   goods: Good[];
 };
 
-export const GoodsList: React.FC<Props> = React.memo(({ goods }) => (
-  <ul>
-    {goods.map(good => (
-      <li key={good.id} data-cy="good" style={{ color: good.color }}>
-        {good.name}
-      </li>
-    ))}
-  </ul>
-));
+export const GoodsList = React.memo(function GoodsList({ goods }: Props) {
+  return (
+    <ul>
+      {goods.map(good => (
+        <li key={good.id} data-cy="good" style={{ color: good.color }}>
+          {good.name}
+        </li>
+      ))}
+    </ul>
+  );
+});
