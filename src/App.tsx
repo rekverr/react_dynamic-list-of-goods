eba4ffbb7dc4b0a4505ee9ba5d2551410c1afc31
Viktor Shchehelskyi <victorshegelsky@gmail.com>
2026-01-28 22:35:32 +0200

    redid it

--- src/App.tsx
+++ src/App.tsx
@@ -6,24 +6,48 @@ import { Good } from './types/Good';
 
 export const App: React.FC = () => {
   const [goods, setGoods] = React.useState<Good[]>([]);
+  const [errorMessage, setErrorMessage] = React.useState('');
 
   return (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
 
-    <button type="button" data-cy="all-button" onClick={() => getAll().then(setGoods)}>
-      Load all goods
-    </button>
+      <button
+        type="button"
+        data-cy="all-button"
+        onClick={() =>
+          getAll()
+            .then(setGoods)
+            .catch(error => setErrorMessage(error.message))
+        }
+      >
+        Load all goods
+      </button>
 
-    <button type="button" data-cy="first-five-button" onClick={() => get5First().then(setGoods)}>
-      Load 5 first goods
-    </button>
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={() =>
+          get5First()
+            .then(setGoods)
+            .catch(error => setErrorMessage(error.message))
+        }
+      >
+        Load 5 first goods
+      </button>
 
-    <button type="button" data-cy="red-button" onClick={() => getRedGoods().then(setGoods)}>
-      Load red goods
-    </button>
-
-    <GoodsList goods={goods} />
-  </div>
+      <button
+        type="button"
+        data-cy="red-button"
+        onClick={() =>
+          getRedGoods()
+            .then(setGoods)
+            .catch(error => setErrorMessage(error.message))
+        }
+      >
+        Load red goods
+      </button>
+      {errorMessage === '' ? <GoodsList goods={goods} /> : <div>{errorMessage}</div>}
+    </div>
   );
-}
+};
